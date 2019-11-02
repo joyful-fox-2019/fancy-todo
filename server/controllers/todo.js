008ac2cd0d78a271b4a02345ab8b32c5f5ecb988
Alif Taufik <alif.tr.aminuddin@gmail.com>
2019-11-02 15:02:02 +0700

    todos creation done. add post save hook to update todos list in user

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,7 +1,17 @@
 const { Todo } = require('../models');
 
 class TodoController {
-	static postNewTodo(req, res, next) {}
+	static postNewTodo(req, res, next) {
+		Todo.create({
+			name: req.body.name,
+			description: req.body.description,
+			user_id: req.body.user_id
+		})
+			.then(todo => {
+				res.status(201).json(todo);
+			})
+			.catch(next);
+	}
 	static getUserTodos(req, res, next) {}
 	static getOneUserTodo(req, res, next) {}
 	static updateTodo(req, res, next) {}
--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,18 +1,37 @@
 const { Schema, model } = require('mongoose');
+const { models } = require('mongoose');
 
 const todoSchema = new Schema({
 	name: {
-		type: String
+		type: String,
+		required: [true, 'Todo Name Required!']
 	},
 	description: {
 		type: String
 	},
 	status: {
-		type: String
+		type: Boolean,
+		default: false
 	},
 	due_date: {
-		type: Date
-	}
+		type: Date,
+		default: new Date()
+	},
+	user_id: { type: Schema.Types.ObjectId, ref: 'User' }
+});
+
+todoSchema.post('save', function(doc, next) {
+	models.User.findByIdAndUpdate(
+		doc.user_id,
+		{ $push: { todos: doc.id } },
+		{ useFindAndModify: false }
+	)
+		.then(user => {
+			next();
+		})
+		.catch(err => {
+			next(err);
+		});
 });
 
 module.exports = model('Todo', todoSchema);
