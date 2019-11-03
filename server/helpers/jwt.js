711adac143f5e4b8e4946db64f1427a2870c474c
dadepe <rizky_dwifutra@yahoo.com>
2019-11-03 16:10:37 +0700

    minor fix

--- server/helpers/jwt.js
+++ server/helpers/jwt.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken")
 
 const generateToken = function(payload){
-    return jwt.sign(payload,process.env.SALT)
+    return jwt.sign(payload,process.env.JWT_SECRET)
 }
 
 const verifyToken = function(token){
-    return jwt.verify(token,process.env.SALT)
+    return jwt.verify(token,process.env.JWT_SECRET)
 }
 
 module.exports = {generateToken, verifyToken}
