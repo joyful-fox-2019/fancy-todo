097ecbb4635c415d62c91c94b10f50b17432efbb
arnold_therigan <arnoldtherigan15@gmail.com>
2019-11-04 06:21:21 +0700

    arnold therigan

--- server/config/simi.js
+++ server/config/simi.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const simi = axios.create({
     baseURL: `https://wsapi.simsimi.com/190410/talk`,
     headers: {
-        "x-api-key": "QoZhtwbxP6Gsiy8irCecrP+I44cNF9q94Te5lvqz",
+        "x-api-key": "nsZahssTALpQf4AgTsael0h5hli3xs/CoH6zaTER",
         "Content-Type": "application/json"
     }
 })
