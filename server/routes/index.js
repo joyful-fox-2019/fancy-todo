3521342a8fd31edb6176622ecdbd3a1850031afd
Royhan Mardista <royhanm23@gmail.com>
2019-11-04 02:30:21 +0700

    sudah

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const todoRouter = require('./todoRouter')
-const userRouter = require('./userRouter')
-
-router.use('/',userRouter)
-router.use('/todo',todoRouter)
-
-
-module.exports = router
